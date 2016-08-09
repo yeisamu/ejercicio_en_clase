4fb25baf0389f462014a7ac8d3224c7361f9c4ee
yeison velasquez <yeison velasquez>
2016-08-08 20:46:51 -0500

    hola mundo con archivos de python

---
+++ HOLAMUNDO.py
@@ -0,0 +1,104 @@
+
+# coding: utf-8
+
+# In[3]:
+
+print "hola mundo"
+
+
+# $$\sum_{i=1}^n{}$$
+
+# # hola mundo esto es un comentario
+# python es sencillo
+
+# $$\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+
+# $$f(x)=li(x)-\sum_{p}{}li(x^p)-log(2)+\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+
+# In[6]:
+
+2+4*6/8.0 -3
+
+
+# In[9]:
+
+3.0/2
+
+
+# In[18]:
+
+abs((3+5j)-(1-1j))
+
+
+# In[16]:
+
+5**3
+
+
+# In[20]:
+
+int(_18)
+
+
+# In[21]:
+
+float(_20)
+
+
+# In[23]:
+
+complex(_6)
+
+
+# In[25]:
+
+round(_18,3)
+
+
+# In[26]:
+
+max(5,9,2,0,56,45)
+
+
+# In[27]:
+
+num=10
+
+
+# In[35]:
+
+x,y,z,w=1,2,3,5
+
+
+# In[36]:
+
+print w
+
+
+# In[37]:
+
+x,y,z,w=z,w,x,y
+
+
+# In[38]:
+
+print x,y,z,w
+
+
+# In[39]:
+
+b=x+y
+
+
+# In[40]:
+
+print b
+
+
+# In[49]:
+
+if x<y:
+    print "el mayor es ",y
+else:
+    print ("el menor es ",x)
+
